55727f13aa3bb01118072bd5ad353af18f049b62
hkimca <harrison.kim.ca@gmail.com>
2017-07-09 12:09:52 -0400

    Initial Commit

---
+++ 2w2d5_fingerPaintAppTests/_w2d5_fingerPaintAppTests.m
@@ -0,0 +1,39 @@
+//
+//  _w2d5_fingerPaintAppTests.m
+//  2w2d5_fingerPaintAppTests
+//
+//  Created by Seantastic31 on 09/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _w2d5_fingerPaintAppTests : XCTestCase
+
+@end
+
+@implementation _w2d5_fingerPaintAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
